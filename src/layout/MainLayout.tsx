2231313e686883d042154897c8831e1bbfc65b9a
Carlos Sluzala <sluzala1@gmail.com>
2025-10-29 15:08:47 -0300

    feat: implement MainLayout component

---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,21 @@
+type MainLayoutProps = React.PropsWithChildren
+
+function Header() {
+  return (
+    <div className="bg-white border border-border text-center">
+      <h1>React Commerce</h1>
+    </div>
+  )
+}
+
+export function MainLayout({ children }: MainLayoutProps) {
+  return (
+    <div className="bg-white border border-border w-screen h-screen">
+      <Header />
+
+      <div>
+        {children}
+      </div>
+    </div>
+  )
+}
